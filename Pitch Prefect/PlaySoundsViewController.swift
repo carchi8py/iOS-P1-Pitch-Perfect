801a2dac76a447fbcbdba93a71717782245b69b6
Chris Archibald <carchi8py@gmail.com>
2015-03-13 21:06:51 -0700

    Add Slow button and function

--- Pitch Prefect/PlaySoundsViewController.swift
+++ Pitch Prefect/PlaySoundsViewController.swift
@@ -7,13 +7,23 @@
 //
 
 import UIKit
+import AVFoundation
 
 class PlaySoundsViewController: UIViewController {
 
+    var audioPlayer:AVAudioPlayer!
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        if var filePath = NSBundle.mainBundle().pathForResource("movie_quote", ofType: "mp3"){
+            var filePathUrl = NSURL.fileURLWithPath(filePath)
+            audioPlayer = AVAudioPlayer(contentsOfURL: filePathUrl, error: nil)
+            audioPlayer.enableRate = true
+            
+        } else {
+            println("the filePath is empty")
+        }
     }
 
     override func didReceiveMemoryWarning() {
@@ -21,6 +31,11 @@ class PlaySoundsViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    @IBAction func playSlowAudio(sender: UIButton) {
+        audioPlayer.stop()
+        audioPlayer.rate = 0.5
+        audioPlayer.play()
+    }
 
     /*
     // MARK: - Navigation
