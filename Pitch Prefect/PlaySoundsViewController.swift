bacc9e75961dd18c22a38f8957b04d5426fba483
Chris Archibald <carchi8py@gmail.com>
2015-03-13 21:09:22 -0700

    Added fast button and functions

--- Pitch Prefect/PlaySoundsViewController.swift
+++ Pitch Prefect/PlaySoundsViewController.swift
@@ -37,6 +37,11 @@ class PlaySoundsViewController: UIViewController {
         audioPlayer.play()
     }
 
+    @IBAction func playFastAudio(sender: UIButton) {
+        audioPlayer.stop()
+        audioPlayer.rate = 1.5
+        audioPlayer.play()
+    }
     /*
     // MARK: - Navigation
 
